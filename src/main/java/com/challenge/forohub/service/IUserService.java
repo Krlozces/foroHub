424f17959071cd29107c047b5e201e363e182a83
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 12:41:35 -0500

    Service user interface created

--- src/main/java/com/challenge/forohub/service/IUserService.java
+++ src/main/java/com/challenge/forohub/service/IUserService.java
@@ -1,4 +1,17 @@
 package com.challenge.forohub.service;
 
+import com.challenge.forohub.entity.User;
+
+import java.util.List;
+
 public interface IUserService {
+    User registrar(User user);
+
+    List<User> users();
+
+    User userById(Long id);
+
+    User actualizar(User user);
+
+    void eliminar(Long id);
 }
