a94b2d28dff4222d6e8ad86ec6ab9ae0f2f04fb4
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 12:42:04 -0500

    Service course interface created

--- src/main/java/com/challenge/forohub/service/ICourseService.java
+++ src/main/java/com/challenge/forohub/service/ICourseService.java
@@ -1,4 +1,18 @@
 package com.challenge.forohub.service;
 
+import com.challenge.forohub.entity.Course;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public interface ICourseService {
+    Course guardar(Course course);
+
+    List<Course> courses();
+
+    Course courseById(Long id);
+
+    Course actualizar(Course course);
+
+    void eliminar(Long id);
 }
