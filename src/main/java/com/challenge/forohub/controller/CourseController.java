6d3e1c2c986da0dd61d12804a077be0d5097844d
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 11:47:32 -0500

    Course controller class implemented

--- src/main/java/com/challenge/forohub/controller/CourseController.java
+++ src/main/java/com/challenge/forohub/controller/CourseController.java
@@ -1,9 +1,13 @@
 package com.challenge.forohub.controller;
 
+import com.challenge.forohub.entity.Course;
 import com.challenge.forohub.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
@@ -13,5 +17,31 @@ public class CourseController {
     @Autowired
     public CourseController(ICourseService iCourseService) {
         this.iCourseService = iCourseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Course>> courses() {
+        return ResponseEntity.status(HttpStatus.OK).body(iCourseService.courses());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> courseDetail(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iCourseService.courseById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Course> updateCourse(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iCourseService.actualizar(course));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id){
+        iCourseService.eliminar(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> saveCourse(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iCourseService.guardar(course));
     }
 }
