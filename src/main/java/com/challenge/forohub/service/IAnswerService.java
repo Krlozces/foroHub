3296cc418cb29ce41f2ffce1f61f6419a57531bb
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 12:42:23 -0500

    Service answer interface created

--- src/main/java/com/challenge/forohub/service/IAnswerService.java
+++ src/main/java/com/challenge/forohub/service/IAnswerService.java
@@ -1,4 +1,17 @@
 package com.challenge.forohub.service;
 
+import com.challenge.forohub.entity.Answer;
+
+import java.util.List;
+
 public interface IAnswerService {
+    Answer saveAnswer(Answer answer);
+
+    List<Answer> answers();
+
+    Answer updateAnswer(Answer answer);
+
+    Answer getById(Long id);
+
+    void deleteAnswer(Long id);
 }
