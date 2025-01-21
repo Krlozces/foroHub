b661573a49bdf7546468e4cf1037405e8eed34f3
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 11:23:21 -0500

    User controller class implemented

--- src/main/java/com/challenge/forohub/controller/UserController.java
+++ src/main/java/com/challenge/forohub/controller/UserController.java
@@ -2,12 +2,11 @@ package com.challenge.forohub.controller;
 
 import com.challenge.forohub.entity.User;
 import com.challenge.forohub.service.IUserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,8 +20,30 @@ public class UserController {
         this.iUserService = iUserService;
     }
 
-    /*@GetMapping
+    @GetMapping
     public ResponseEntity<List<User>> listUsers() {
-        return ResponseEntity.status(HttpStatus.OK).body(iUserService.);
-    }*/
+        return ResponseEntity.status(HttpStatus.OK).body(iUserService.users());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iUserService.userById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iUserService.actualizar(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        iUserService.eliminar(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iUserService.registrar(user));
+    }
+
 }
