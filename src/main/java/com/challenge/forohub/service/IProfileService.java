43208004d28e3db1a5b1aa3310d763cc594d2665
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 12:40:50 -0500

    Service profile interface created

--- src/main/java/com/challenge/forohub/service/IProfileService.java
+++ src/main/java/com/challenge/forohub/service/IProfileService.java
@@ -1,4 +1,17 @@
 package com.challenge.forohub.service;
 
+import com.challenge.forohub.entity.Profile;
+
+import java.util.List;
+
 public interface IProfileService {
+    Profile saveProfile(Profile profile);
+
+    List<Profile> profiles();
+
+    Profile updateProfile(Profile profile);
+
+    Profile getById(Long id);
+
+    void deleteProfile(Long id);
 }
