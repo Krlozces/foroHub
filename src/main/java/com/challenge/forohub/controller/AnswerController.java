f26e13ed8a86c08524139d2b4b8a37a9c4e9a678
Brayan Cespedes <bryancespedes1706@gmail.com>
2025-01-21 12:22:51 -0500

    Answer controller class implemented

--- src/main/java/com/challenge/forohub/controller/AnswerController.java
+++ src/main/java/com/challenge/forohub/controller/AnswerController.java
@@ -1,9 +1,13 @@
 package com.challenge.forohub.controller;
 
+import com.challenge.forohub.entity.Answer;
 import com.challenge.forohub.service.IAnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/answers")
@@ -13,5 +17,31 @@ public class AnswerController {
     @Autowired
     public AnswerController(IAnswerService iAnswerService) {
         this.iAnswerService = iAnswerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Answer>> answers() {
+        return ResponseEntity.status(HttpStatus.OK).body(iAnswerService.answers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Answer> answerDetails(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iAnswerService.getById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Answer> updateAnswer(@RequestBody Answer answer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iAnswerService.updateAnswer(answer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAnswer(@PathVariable Long id) {
+        iAnswerService.deleteAnswer(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Answer> saveAnswer(@RequestBody Answer answer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(iAnswerService.saveAnswer(answer));
     }
 }
